ec6fdf886ce42f7c637691bf99670dfa7e205bfe
huanghyul <1103221645@qq.com>
2023-08-10 20:09:06 +0800

    save

--- 02.基础算法/01.数组/main.go
+++ 02.基础算法/01.数组/main.go
@@ -6,6 +6,16 @@ func f1() {
 	print(arr[3]) // 4
 }
 
+// 访问越界
+func f2() {
+	arr := []int{1, 2, 3, 4, 5}
+	for i := 0; i <= 4; i++ {
+		arr[i] = 0
+		print("hello world")
+	}
+}
+
 func main() {
-	f1()
+	// f1()
+	f2()
 }
